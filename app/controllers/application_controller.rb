44387fdbf9b3f328ed8536d4e0a8ee4754e1a9a3
Chance Smith <chancesmith97@hotmail.com>
2019-12-06 03:34:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
       
     if @user.class == User 
       session[:user_id] = @user.id 
-      redirect to '/account'
+      redirect_to '/account'
     else
       erb :error
    end
